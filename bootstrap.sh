c23f37707b35fbf667cbccadc681cbe6e1b17a9a
Yaroslav Yavornytskyi <ya.yaroslav27@gmail.com>
2024-06-07 19:58:34 +0200

    Solution

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+# Create namespace
+kubectl apply -f .infrastructure/namespace.yml
+
+# Apply PersistentVolume
+kubectl apply -f .infrastructure/pv.yml
+
+
+# Apply PersistentVolumeClaim
+kubectl apply -f .infrastructure/pvc.yml -n todoapp
+
+# Apply ConfigMap
+kubectl apply -f .infrastructure/configmap.yml -n todoapp
+
+# Apply Secret
+kubectl apply -f .infrastructure/secret.yml -n todoapp
+
+# Apply Deployment
+kubectl apply -f .infrastructure/deployment.yml -n todoapp
+
+echo "All resources have been deployed successfully."
